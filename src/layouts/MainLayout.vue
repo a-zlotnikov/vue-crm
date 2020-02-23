a461d628fcb81e0d33eef9264f68136780cd51e3
Анатолий Злотников <a.v.zlotnikov@gmail.com>
2020-02-23 12:27:27 +0300

    Release

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="app-main-layout">
+
+      <Navbar @click="isOpen = !isOpen"/>
+
+      <Sidebar v-model="isOpen"/>
+
+      <main class="app-content" :class="{full: !isOpen}">
+        <div class="app-page">
+          <router-view />
+        </div>
+      </main>
+
+      <div class="fixed-action-btn">
+        <a class="btn-floating btn-large blue" href="#">
+          <i class="large material-icons">add</i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from '@/components/app/Navbar'
+  import Sidebar from '@/components/app/Sidebar'
+  export default {
+    name: 'MainLayout',
+    components: {
+      Navbar, Sidebar
+    },
+    data: () => ({
+      isOpen: true
+    })
+  };
+</script>
+
+<style scoped>
+
+</style>
